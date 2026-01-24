f56b21cc7be2d7f33fd63d9c0d9ea2c0baf11699
dakribe <dakribe94@gmail.com>
2026-01-24 10:15:27 -0600

    rust_analyzer lsp

---
+++ .config/nvim/lsp/rust_analyzer.lua
@@ -0,0 +1,11 @@
+---@type vim.lsp.Config
+return {
+	cmd = { 'rust-analyzer' },
+	filetypes = { 'rust' },
+	root_markers = { 'Cargo.toml', 'rust-project.json' },
+	settings = {
+		['rust-analyzer'] = {
+			inlayHints = {},
+		},
+	},
+}
