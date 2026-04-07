053a8d644552f6ba7aaddfd0472f6be6efb9f17f
dakribe <dakribe94@gmail.com>
2026-04-07 05:32:53 -0500

    remove fidget.nvim

--- .config/nvim/lua/plugins/mini.notify.lua
+++ .config/nvim/lua/plugins/mini.notify.lua
@@ -1,9 +1,18 @@
+local win_config = function()
+	local has_statusline = vim.o.laststatus > 0
+	local pad = vim.o.cmdheight + (has_statusline and 1 or 0)
+	return { anchor = 'SE', col = vim.o.columns, row = vim.o.lines - pad }
+end
+
 return {
 	'nvim-mini/mini.notify',
 	version = false,
 	opts = {
 		lsp_progress = {
-			enable = false,
+			enable = true,
+		},
+		window = {
+			config = win_config,
 		},
 	},
 }
