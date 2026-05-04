2a9dd2a805756c40844ce5929710afddf6a225a9
dakribe <dakribe94@gmail.com>
2026-05-03 19:07:37 -0500

    svelte lsp

---
+++ .config/nvim/lsp/svelte.lua
@@ -0,0 +1,11 @@
+---@type vim.lsp.Config
+return {
+   cmd = { 'svelteserver', '--stdio' },
+   filetypes = { 'svelte' },
+   root_markers = { 'svelte.config.js', 'svelte.config.ts', 'package.json' },
+   settings = {
+      svelte = {
+         enable = true,
+      },
+   },
+}
