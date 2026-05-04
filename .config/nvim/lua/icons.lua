fee84ddd878f6b3aa0049996656fb314c6970257
dakribe <dakribe94@gmail.com>
2026-05-03 19:08:50 -0500

    add icons

--- .config/nvim/lua/icons.lua
+++ .config/nvim/lua/icons.lua
@@ -7,4 +7,40 @@ M.diagnostics = {
 	INFO = ' ',
 }
 
+M.arrows = {
+	right = '',
+	left = '',
+	up = '',
+	down = '',
+}
+
+M.symbol_kinds = {
+	Array = '󰅪',
+	Class = '',
+	Color = '󰏘',
+	Constant = '󰏿',
+	Constructor = '',
+	Enum = '',
+	EnumMember = '',
+	Event = '',
+	Field = '󰜢',
+	File = '󰈙',
+	Folder = '󰉋',
+	Function = '󰆧',
+	Interface = '',
+	Keyword = '󰌋',
+	Method = '󰆧',
+	Module = '',
+	Operator = '󰆕',
+	Property = '󰜢',
+	Reference = '󰈇',
+	Snippet = '',
+	Struct = '',
+	Text = '',
+	TypeParameter = '',
+	Unit = '',
+	Value = '',
+	Variable = '󰀫',
+}
+
 return M
