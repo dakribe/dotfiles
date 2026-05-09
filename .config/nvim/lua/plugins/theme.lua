5d60da21dd7f5b2f5e14c03012a18060bfe28caa
dakribe <dakribe94@gmail.com>
2026-05-09 16:18:11 -0500

    pin theme to commit

--- .config/nvim/lua/plugins/theme.lua
+++ .config/nvim/lua/plugins/theme.lua
@@ -1,10 +1,24 @@
 return {
 	'jpwol/thorn.nvim',
+	commit = '3116f57',
 	lazy = false,
 	priority = 1000,
 	config = function()
 		require('thorn').setup({
 			background = 'cold',
+			styles = {
+				keywords = { italic = false, bold = false },
+				strings = { italic = false, bold = false },
+				comments = { italic = false, bold = false },
+				diagnostic = {
+					underline = false,
+
+					error = { highlight = false },
+					hint = { highlight = true },
+					info = { highlight = false },
+					warn = { highlight = false },
+				},
+			},
 		})
 		vim.cmd([[colorscheme thorn]])
 	end,
